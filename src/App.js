9794f653f1dc3f3ce0edaeb5cd2d410a5e430435
Andrew Caygill <andy8730@googlemail.com>
2019-10-03 11:39:32 -0700

    Removing unused code from note detection script, adding polyfill for AnalyserNode.getFloatTimeDomainData

--- src/App.js
+++ src/App.js
@@ -3,13 +3,26 @@ import Pedal from "./components/pedal-svg/pedal.js";
 import { getNoteData } from "./utils/note-detection/index.js";
 import "./App.css";
 
+if (
+	global.AnalyserNode &&
+	!global.AnalyserNode.prototype.getFloatTimeDomainData
+) {
+	var uint8 = new Uint8Array(2048);
+	global.AnalyserNode.prototype.getFloatTimeDomainData = function(array) {
+		this.getByteTimeDomainData(uint8);
+		for (var i = 0, imax = array.length; i < imax; i++) {
+			array[i] = (uint8[i] - 128) * 0.0078125;
+		}
+	};
+}
+
 function App() {
 	const [currentNote, setCurrentNote] = useState();
 	const [needlePosition, setNeedlePosition] = useState();
 
-	/*
-	function calculateNeedlePosition(noteData) {
-		const currentNoteFrequency = noteData.currentNoteFrequency;
+	function calculateNeedlePosition(noteVariance) {
+		return noteVariance * 20;
+		/*const currentNoteFrequency = noteData.currentNoteFrequency;
 		const noteMinFrequency = noteData.noteMinFrequency;
 		const noteMaxFrequency = noteData.noteMaxFrequency;
 
@@ -19,40 +32,53 @@ function App() {
 		const currentFreq = currentNoteFrequency - noteMinFrequency;
 		const percentage = (currentFreq * 100) / maxFreq;
 
-		return percentage;
-  }
-  */
+    return percentage;
+    */
+	}
 
 	function successCallback(stream) {
 		//window.AudioContext = window.AudioContext || window.webkitAudioContext;
-		const audioContext = new AudioContext();
+		const audioContext = new (window.AudioContext ||
+			window.webkitAudioContext)();
 		const analyser = audioContext.createAnalyser();
 		analyser.fftSize = Math.pow(2, 13);
 		const sampleRate = audioContext.sampleRate;
 		const data = new Float32Array(analyser.fftSize);
 
 		function step() {
-			requestAnimationFrame(step);
 			analyser.getFloatTimeDomainData(data);
 
 			const noteData = getNoteData(data, sampleRate);
 
 			if (noteData) {
+				console.log(noteData);
 				setCurrentNote(noteData.key);
-				setNeedlePosition(noteData.variance);
+				setNeedlePosition(calculateNeedlePosition(noteData.variance));
 			}
+
+			//requestAnimationFrame(step);
 		}
+
+		setInterval(step, 50);
 
 		const mediaStreamSource = audioContext.createMediaStreamSource(stream);
 		mediaStreamSource.connect(analyser);
-		requestAnimationFrame(step);
+		//requestAnimationFrame(step);
 	}
 
 	function errorCallback(err) {
 		alert(err);
 	}
 
-	navigator.getUserMedia({ audio: true }, successCallback, errorCallback);
+	//navigator.getUserMedia(, , errorCallback);
+	navigator.mediaDevices
+		.getUserMedia({ audio: true })
+		.then(function(stream) {
+			successCallback(stream);
+		})
+		.catch(function(err) {
+			alert(err);
+		});
 
 	let audioContext = new (window.AudioContext || window.webkitAudioContext)();
 	let analyserNode = audioContext.createAnalyser();
--- src/utils/note-detection/index.js
+++ src/utils/note-detection/index.js
@@ -2,66 +2,30 @@
 import NoteFrequencies from "./note-frequencies.js";
 import YinJs from "yinjs/yin.js";
 
-// Lookup array for note names.
-const keys = ["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"];
+const BASE_NOTE = 440.0 * Math.pow(2.0, -4.75); // Octave 0, Key C
+const KEYS = ["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"];
 
-function getNextNote(note) {
-	if (note === "B") {
-		return "C";
-	} else {
-		const currentNoteIndex = keys.indexOf(note);
-		return keys[currentNoteIndex + 1];
-	}
-}
-
-function getPrevNote(note) {
-	if (note === "C") {
-		return "B";
-	} else {
-		const currentNoteIndex = keys.indexOf(note);
-		return keys[currentNoteIndex - 1];
-	}
-}
-
-function getNoteRange(octave, note) {
-	const nextNote = getNextNote(note);
-	const prevNote = getPrevNote(note);
-	const currentNoteFrequency = NoteFrequencies[octave][note];
-	const nextNoteFrequency = NoteFrequencies[octave][nextNote];
-	const prevNoteFrequency = NoteFrequencies[octave][prevNote];
-
-	const noteMinFrequency =
-		currentNoteFrequency - Math.abs(currentNoteFrequency - prevNoteFrequency);
-	const noteMaxFrequency =
-		currentNoteFrequency + Math.abs(currentNoteFrequency - nextNoteFrequency);
-
-	return { currentNoteFrequency, noteMinFrequency, noteMaxFrequency };
+function getKeyandOctave(frequency) {
+	const halfStepsBelowMiddleC = Math.round(
+		12.0 * Math.log2(frequency / BASE_NOTE)
+	);
+	const octave = Math.floor(halfStepsBelowMiddleC / 12.0);
+	const key = KEYS[Math.floor(halfStepsBelowMiddleC % 12)];
+	return { octave, key };
 }
 
 export function getNoteData(data, sampleRate) {
 	const frequency = window.yin(data, sampleRate);
 	if (frequency) {
-		const c0 = 440.0 * Math.pow(2.0, -4.75);
-		const halfStepsBelowMiddleC = Math.round(12.0 * Math.log2(frequency / c0));
-		const remainder = (12.0 * Math.log2(frequency / c0)) % 1;
-		const remainderPercent = remainder * 100;
-		const variance = Math.abs(remainderPercent - 100);
-		const octave = Math.floor(halfStepsBelowMiddleC / 12.0);
-		const key = keys[Math.floor(halfStepsBelowMiddleC % 12)];
-
-		if (octave > 0) {
-			const {
-				currentNoteFrequency,
-				noteMinFrequency,
-				noteMaxFrequency
-			} = getNoteRange(octave, key);
+		const { octave, key } = getKeyandOctave(frequency);
+		if (octave && key) {
+			const currentNotePerfectFrequency = NoteFrequencies[octave][key];
+			const variance = frequency - currentNotePerfectFrequency;
 
 			return {
 				octave,
 				key,
-				currentNoteFrequency,
-				noteMinFrequency,
-				noteMaxFrequency,
+				frequency,
 				variance
 			};
 		}
